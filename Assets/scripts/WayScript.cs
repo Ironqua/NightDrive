133252f7a2c0e0c7b64c371835ef5213a0e4ae48
Berk KOÇ <114105582+Ironqua@users.noreply.github.com>
2024-04-07 11:39:38 +0300

    Update WayScript.cs

--- Assets/scripts/WayScript.cs
+++ Assets/scripts/WayScript.cs
@@ -6,11 +6,12 @@ public class WayScript : MonoBehaviour
 {
     public GameObject wayprefab;
     bool waycontrol;
-
+[SerializeField] Transform[] roads;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag=="mainCar"&& waycontrol==false) { 
+        /*
             Vector3 spawnpoint = new Vector3(transform.position.x, transform.position.y + 8, 0f);
         Instantiate(wayprefab, spawnpoint, Quaternion.identity);
 
@@ -18,6 +19,15 @@ public class WayScript : MonoBehaviour
 
             Destroy(wayprefab, 7f);
             waycontrol = true;
+*/
+
+#region 
+        roads[0].localPosition+=new Vector3(0,roads[0].localScale.y*yPos,0);
+        Array.Reverse(roads);
+        #endregion
+
+
+            
     }
     }
 
